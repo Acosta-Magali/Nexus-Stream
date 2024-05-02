3e107bc4dea18bc33185c62112450e264652b0ca
Acosta-Magali <magaliacosta.r@gmail.com>
2024-05-02 12:01:32 -0300

    arreglos en carrusel

--- Nexus-Stream/js/main.js
+++ Nexus-Stream/js/main.js
@@ -1,10 +1,12 @@
 const fila = document.querySelector('.contenedor-carousel');
 const peliculas = document.querySelectorAll('.pelicula');
 
+
 const flechaIzquierda = document.getElementById('flecha-izquierda');
 const flechaDerecha = document.getElementById('flecha-derecha');
 
-// ? ----- ----- Event Listener para la flecha derecha. ----- -----
+
+// ?  Event Listener para la flecha derecha.
 flechaDerecha.addEventListener('click', () => {
 	fila.scrollLeft += fila.offsetWidth;
 
@@ -15,7 +17,7 @@ flechaDerecha.addEventListener('click', () => {
 	}
 });
 
-// ? ----- ----- Event Listener para la flecha izquierda. ----- -----
+// ?  Event Listener para la flecha izquierda. ----- -----
 flechaIzquierda.addEventListener('click', () => {
 	fila.scrollLeft -= fila.offsetWidth;
 
@@ -24,6 +26,7 @@ flechaIzquierda.addEventListener('click', () => {
 		indicadorActivo.previousSibling.classList.add('activo');
 		indicadorActivo.classList.remove('activo');
 	}
+
 });
 
 // ? ----- ----- Paginacion ----- -----
@@ -35,6 +38,7 @@ for(let i = 0; i < numeroPaginas; i++){
 		indicador.classList.add('activo');
 	}
 
+
 	document.querySelector('.indicadores').appendChild(indicador);
 	indicador.addEventListener('click', (e) => {
 		fila.scrollLeft = i * fila.offsetWidth;
@@ -42,6 +46,7 @@ for(let i = 0; i < numeroPaginas; i++){
 		document.querySelector('.indicadores .activo').classList.remove('activo');
 		e.target.classList.add('activo');
 	});
+
 }
 
 // ? ----- ----- Hover ----- -----
