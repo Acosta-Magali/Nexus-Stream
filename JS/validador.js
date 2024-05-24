6bb9f61aeee1458c0f8ed89139b9cbbf89ffec31
Milagros <163451640+Mtbm88@users.noreply.github.com>
2024-05-24 20:32:41 -0300

    Ya pude recuperar los archivos que borré, perdon xd

---
+++ JS/validador.js
@@ -0,0 +1,38 @@
+
+
+        function useRegex(input) {
+            let regex = /[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+(?:\.[-A-Za-z0-9!#$%&'*+\/=?^_`{|}~]+)*@(?:[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?\.)+[A-Za-z0-9](?:[-A-Za-z0-9]*[A-Za-z0-9])?/i;
+            return regex.test(input);
+        }
+
+        let largo_contrasenia = 8;
+
+        function testInputs() {
+
+            email = document.getElementById('email').value;
+            password = document.getElementById('password').value;
+
+            if ( useRegex(email) && password.length >= largo_contrasenia ) {
+
+       
+
+                window.open('/completatusdatos.html')
+
+            }
+
+        }
+        function testInputs2() {
+
+            email = document.getElementById('email').value;
+            password = document.getElementById('password').value;
+
+            if ( useRegex(email) && password.length >= largo_contrasenia ) {
+
+          
+
+                window.open('/Nexus-Stream/perfiles.html')
+
+            }
+
+        }
+    
